093054004ecd9cc742d52baab340199c5081b3c0
penguinpwn <peng.penguinited@gmail.com>
2026-02-28 00:45:27 +0330

    pushed

--- app/config.py
+++ app/config.py
@@ -3,11 +3,15 @@ import os
 
 class Config:
     SECRET_KEY = secrets.token_bytes(32)
+
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:"
+        "mssql+pyodbc://"
+        f"{os.getenv('DB_USER')}:"
         f"{os.getenv('DB_PASS')}@"
         f"{os.getenv('DB_HOST')}/"
         f"{os.getenv('DB_NAME')}"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_SECRET_KEY = secrets.token_bytes(32)
--- app/models.py
+++ app/models.py
@@ -9,16 +9,19 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'users2'
     user_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), nullable=False)
-    role = db.Column(db.Enum('admin', 'client'), nullable=False)
+    role = db.Column(
+        db.String(20),
+        CheckConstraint("role IN ('admin', 'client')"),
+        nullable=False
+    )
     is_active = db.Column(db.Boolean, default=True, nullable=False)
     name = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
     auth = db.relationship("Auth", backref="user", uselist=False)
     tokens = db.relationship("TokenServices", backref="user", cascade="all, delete-orphan")
-    admin = db.relationship("Admins", backref="user", uselist=False)
     client = db.relationship("Clients", backref="user", uselist=False)
     users_info = db.relationship("UsersInfo", backref="user", uselist=False)
     admin = db.relationship(
@@ -38,7 +41,7 @@ class Users(db.Model):
 
 class Auth(db.Model):
     __tablename__ = 'auth'
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
@@ -49,18 +52,22 @@ class Auth(db.Model):
 class TokenServices(db.Model):
     __tablename__ = 'token_services'
     token_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), index=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), index=True, nullable=False)
     token_hash = db.Column(db.String(64), nullable=False, unique=True)
-    token_type = db.Column(db.Enum('access', 'refresh'), nullable=False)
+    token_type = db.Column(
+        db.String(20),
+        CheckConstraint("token_type IN ('access','refresh')", name="chk_token_type"),
+        nullable=False
+    )
     revoked = db.Column(db.Boolean, default=False, nullable=False)
     expires_at = db.Column(db.DateTime, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
 class Admins(db.Model):
     __tablename__ = 'admins'
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
     permission_level = db.Column(db.Integer, nullable=False, default=1)
-    granted_by = db.Column(db.String(36), db.ForeignKey('users.user_id'), nullable=True)
+    granted_by = db.Column(db.String(36), db.ForeignKey('users2.user_id'), nullable=True)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
     __table_args__ = (
@@ -72,22 +79,43 @@ class Admins(db.Model):
     
 class Clients(db.Model):
     __tablename__ = 'clients'
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
 
 class UsersInfo(db.Model):
     __tablename__ = 'users_info'
 
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), primary_key=True, nullable=False)
     profile_photo_key = db.Column(db.String(255), nullable=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=True)
-    department = db.Column(db.Enum('software_development','qa','devops','hr','finance','sales', name='department_enum'), nullable=False)
-    designation = db.Column(db.Enum('junior_developer','developer','senior_developer','tech_lead','engineering_manager', name='designation_enum'), nullable=False)
+    department = db.Column(
+        db.String(50),
+        CheckConstraint(
+            "department IN ('software_development','qa','devops','hr','finance','sales')",
+            name="chk_department"
+        ),
+        nullable=False  
+    )
+    designation = db.Column(
+        db.String(50),
+        CheckConstraint(
+            "designation IN ('junior_developer','developer','senior_developer','tech_lead','engineering_manager')",
+            name="chk_designation"
+        ),
+        nullable=False
+    )
     phone = db.Column(db.String(15), nullable=False, unique=True)
     employee_id = db.Column(db.String(20), nullable=False, unique=True)
-    gender = db.Column(db.Enum('male', 'female', name='gender_enum'), nullable=False)
+    gender = db.Column(
+        db.String(10),
+        CheckConstraint(
+            "gender IN ('male','female')",
+            name="chk_gender"
+        ),
+        nullable=False
+    )
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     updated_at = db.Column( db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
 
@@ -96,13 +124,28 @@ class LeaveRequests(db.Model):
     __tablename__ = 'leave_requests'
 
     leave_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), index=True, nullable=False)
-    leave_type = db.Column(db.Enum('casual_leave', 'sick_leave', 'annual_leave', 'emergency_leave', name='leave_type_enum'), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), index=True, nullable=False)
+    leave_type = db.Column(
+        db.String(30),
+        CheckConstraint(
+            "leave_type IN ('casual_leave','sick_leave','annual_leave','emergency_leave')",
+            name="chk_leave_type"
+        ),
+        nullable=False
+    )
     from_date = db.Column(db.Date, nullable=False)
     to_date = db.Column(db.Date, nullable=False)
     days = db.Column(db.Integer, nullable=False)
     reason = db.Column(db.Text, nullable=False)
-    status = db.Column(db.Enum('pending', 'approved', 'rejected', name='leave_status_enum'), nullable=False, default='pending')
+    status = db.Column(
+        db.String(20),
+        CheckConstraint(
+            "status IN ('pending','approved','rejected')",
+            name="chk_leave_status"
+        ),
+        nullable=False,
+        default='pending'
+    )
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
 
@@ -113,11 +156,19 @@ class Attendance(db.Model):
     __tablename__ = 'attendance'
 
     attendance_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.user_id', ondelete='CASCADE'), index=True, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users2.user_id', ondelete='CASCADE'), index=True, nullable=False)
     date = db.Column(db.Date, nullable=False)
     clock_in = db.Column(db.DateTime, nullable=False)
     clock_out = db.Column(db.DateTime, nullable=True)
-    status = db.Column(db.Enum('present', 'absent', 'half_day', 'late', name='attendance_status_enum'), nullable=False, default='present')
+    status = db.Column(
+        db.String(20),
+        CheckConstraint(
+            "status IN ('present','absent','half_day','late')",
+            name="chk_attendance_status"
+        ),
+        nullable=False,
+        default='present'
+    )
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
 
